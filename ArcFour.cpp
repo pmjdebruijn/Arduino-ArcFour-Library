fba74d78c6bd313578df814e5651520d75471331
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2014-06-10 18:25:35 +0200

    Initial code dump

---
+++ ArcFour.cpp
@@ -0,0 +1,54 @@
+/*
+  ArcFour.cpp
+  
+  created 10 Jun 2014
+  by Pascal de Bruijn
+  
+ */
+
+#include "Arduino.h"
+#include "ArcFour.h"
+
+void ArcFour::initialize(void)
+{
+  for (int i = 0; i < sizeof(_s); i++)
+  {
+    _s[i] = i;
+  }
+  
+  _i = 0;
+  _j = 0;
+}
+
+void ArcFour::seed(uint8_t index, uint8_t key)
+{
+  _j = (_j + _s[index] + key) % sizeof(_s);
+  
+  swap(&_s[index], &_s[_j]);
+}
+
+void ArcFour::finalize(void)
+{
+  _i = 0;
+  _j = 0;
+}
+
+byte ArcFour::random(void)
+{
+  _i = (_i + 1)      % sizeof(_s);
+  _j = (_j + _s[_i]) % sizeof(_s);
+  
+  swap(&_s[_i], &_s[_j]);
+  
+  return _s[( _s[_i] + _s[_j]) % sizeof(_s)];
+}
+
+void ArcFour::swap(uint8_t *a, uint8_t *b)
+{
+  uint8_t c;
+  
+   c = *a;
+  *a = *b;
+  *b =  c;
+}
+
---
+++ ArcFour.h
@@ -0,0 +1,29 @@
+/*
+  ArcFour.h
+  
+  created 10 Jun 2014
+  by Pascal de Bruijn
+  
+ */
+
+#ifndef ArcFour_h
+#define ArcFour_h
+
+#define ARCFOUR_MAX 256
+
+class ArcFour
+{
+  uint8_t _i;
+  uint8_t _j;
+  uint8_t _s[ARCFOUR_MAX];
+public:
+  void initialize(void);
+  void seed(uint8_t index, uint8_t key);
+  void finalize(void);
+  byte random(void);
+private:
+  void swap(uint8_t *a, uint8_t *b);
+};
+
+#endif
+
